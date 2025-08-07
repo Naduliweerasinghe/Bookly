1c25e5d4b2d4245093b403de07f1a590660b8043
Naduliweerasinghe <naduliweerasinghe@gmail.com>
2025-08-07 22:11:48 +0530

    develop backend

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import "dotenv/config";
+
+import authRoutes from "./routes/authRoutes.js";
+import { connectDB } from "./lib/db.js";
+import { connect } from "mongoose";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use('/api/auth',authRoutes)
+
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ${PORT}');
+    connectDB();
+});
+
