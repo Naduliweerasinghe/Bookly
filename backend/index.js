088eb4eb4566dd691fcaba5bd47267da3f5561f5
Naduliweerasinghe <naduliweerasinghe@gmail.com>
2025-09-07 16:20:36 +0530

    Complete Backend task

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,29 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
+
 import { connectDB } from "./lib/db.js";
 import { connect } from "mongoose";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 
-app.use('/api/auth',authRoutes)
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/auth',authRoutes);
+app.use('/api/books',bookRoutes);
 
 
 app.listen(PORT, () => {
-    console.log('Server is running on port ${PORT}');
+    console.log(`Server is running on port ${PORT}`);
     connectDB();
 });
 
